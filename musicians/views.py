c0bc9e4a1167fc4f5308bb1c133d0b9ba830bfd5
Evgeny <chapayevkrim@rambler.ru>
2022-10-13 10:21:03 +0300

    Update form

--- musicians/views.py
+++ musicians/views.py
@@ -39,17 +39,12 @@ def about(request):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
-            # print(form.cleaned_data)
-            try:
-                Musicians.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
-
     return render(request, 'musicians/addpage.html', {'form': menu, 'title': 'Добавление статьи', 'form': form})
 
 
