2c96fdc63f7e113bdc0b058d318ac3a5d2a768b5
Evgeny <chapayevkrim@rambler.ru>
2022-10-15 07:34:05 +0300

    Added: caching

--- musicians/urls.py
+++ musicians/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
 urlpatterns = [
+    # path('', cache_page(60)(MusiciansHome.as_view()), name='home'),
     path('', MusiciansHome.as_view(), name='home'),
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
--- musicians/utils.py
+++ musicians/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-
+from django.core.cache import cache
 from .models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -12,6 +12,10 @@ class DataMixin:
     paginate_by = 2
     def get_user_context(self, **kwargs):
         context = kwargs
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('musicians'))
+            cache.set('cats', cats, 60)
         cats = Category.objects.annotate(Count('musicians'))
 
         user_menu = menu.copy()
